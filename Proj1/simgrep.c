ccfaf09665d748768f7726b502649d4f194f850b
Daniela João <up201505982@fe.up.pt>
2021-03-01 14:30:03 +0000

    final

---
+++ Proj1/simgrep.c
@@ -0,0 +1,335 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <signal.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <regex.h>
+
+
+#define _GNU_SOURCE
+
+struct Flags{
+  int iMode;
+  int lMode;
+  int nMode;
+  int cMode;
+  int wMode;
+  int rMode;
+  int fileNameNr;
+  int patternNr;
+  char fileName[];
+} flags;
+
+
+void sigint_handler(int signo){
+
+  char terminate;
+
+  kill(getppid(),SIGSTOP);
+
+  printf("\n> DO YOU WISH TO TERMINATE?\n");
+  scanf("%s",&terminate);
+
+
+  terminate = (char)toupper(terminate);
+
+  if(terminate == 'N'){
+    kill(getppid(),SIGCONT);
+
+  }
+  else{
+    kill(getppid(),SIGQUIT);
+    kill(getpid(),SIGQUIT);
+  }
+
+}
+void subscribe_SIGINT(){
+
+  struct sigaction act;
+  act.sa_handler = sigint_handler;
+  act.sa_flags = 0;
+  sigemptyset(&act.sa_mask);
+
+  sigaction(SIGINT,&act,NULL);
+
+}
+
+void processArgs(int argc, char* argv[]){
+
+  flags.iMode = 0;
+  flags.lMode = 0;
+  flags.nMode = 0;
+  flags.cMode = 0;
+  flags.wMode = 0;
+  flags.rMode = 0;
+  flags.fileNameNr=2;
+  flags.patternNr=1;
+  //flags.fileName=argv[argc];
+  //flags.pattern=argv[argc-1];
+
+  for(size_t i = 0; i < argc; i++){
+    if (strcmp(argv[i],"-i") == 0) {flags.iMode = 1; flags.fileNameNr++; flags.patternNr++;}
+    if (strcmp(argv[i],"-l") == 0) {flags.lMode = 1; flags.fileNameNr++; flags.patternNr++;}
+    if (strcmp(argv[i],"-n") == 0) {flags.nMode = 1; flags.fileNameNr++; flags.patternNr++;}
+    if (strcmp(argv[i],"-c") == 0) {flags.cMode = 1; flags.fileNameNr++; flags.patternNr++;}
+    if (strcmp(argv[i],"-w") == 0) {flags.wMode = 1; flags.fileNameNr++; flags.patternNr++;}
+    if (strcmp(argv[i],"-r") == 0) {flags.rMode = 1; flags.fileNameNr++; flags.patternNr++;}
+  }
+  strcpy(flags.fileName, argv[flags.fileNameNr]);
+
+}
+
+int searchInFile(char *fname, char *str) {
+  FILE *fp;
+  int line_num = 1;
+  int find_result = 0;
+  int lineCount = 0;
+  char temp[512];
+/*
+  regex_t regex;
+  int ptrRegex;
+
+  if(flags.wMode){
+
+    char wModeString[50];
+    strcpy(wModeString,"(\W)"); //(?:^|\W)
+    strcat(wModeString,str);
+    strcat(wModeString,"(\W)"); //(?:$|\W)
+
+    //printf("%s\n", wModeString);
+    ptrRegex = regcomp(&regex, wModeString, 0);
+    if (ptrRegex) {
+      printf("Could not compile regex\n");
+      exit(1);
+    }
+  }
+*/
+
+/*TESTES
+  printf("flags.iMode:%d\n",flags.iMode);
+  printf("flags.lMode:%d\n",flags.lMode);
+  printf("flags.nMode:%d\n",flags.nMode);
+  printf("flags.cMode:%d\n",flags.cMode);
+  printf("flags.wMode:%d\n",flags.wMode);
+  printf("flags.rMode:%d\n",flags.rMode);
+*/
+  if((fp = fopen(fname, "r")) == NULL) {
+  	printf("simgrep: %s: No such file or directory\n",fname);
+    return(-1);
+  }
+
+  if(flags.wMode){
+    char *token;
+	  while(fgets(temp, 512, fp) != NULL) {
+      char auxTemp[512];
+      strcpy(auxTemp,temp);
+      token = strtok(temp, " ,.-\n");
+      while(token!=NULL){
+        if(!flags.iMode){
+          if(strcmp(token,str) == 0) {
+            if(flags.lMode){
+              printf("%s \n", fname);
+              break;
+            }
+            else if(flags.cMode)
+            {
+              lineCount++;
+            }
+            else
+            {
+              if(flags.nMode){ printf("%d:",line_num);}
+              printf("%s", auxTemp);
+            }
+            find_result++;
+
+          }
+        }
+        else
+        {	if(strcasecmp(token,str) == 0) {
+            if(flags.lMode){
+              printf("%s \n", fname);
+              break;
+            }
+            else if(flags.cMode)
+            {
+              lineCount++;
+            }
+            else
+            {
+              if(flags.nMode){ printf("%d:",line_num);}
+              printf("%s", auxTemp);
+            }
+            find_result++;
+
+        }
+
+      }
+      token = strtok(NULL, " ,.-\n");
+    }
+    line_num++;
+  }
+
+  if(flags.cMode ){ printf("%d \n",lineCount);}
+
+}
+
+
+  if(flags.iMode){
+	  while(fgets(temp, 512, fp) != NULL) {
+	    if((strcasestr(temp, str)) != NULL) {
+
+	      if(flags.lMode){
+			     	printf("%s \n", fname);
+			      break;
+			    }
+			    else if(flags.cMode)
+			    {
+			     	lineCount++;
+			    }
+			    else
+			    {
+			      if(flags.nMode){ printf("%d:",line_num);}
+			     	printf("%s", temp);
+			    }
+
+			    find_result++;
+
+		    }
+
+		 	line_num++;
+		}
+
+	 if(flags.cMode ){ printf("%d \n",lineCount);}
+
+	}
+	 else
+	{
+    if(flags.wMode){
+      //REGEX
+    }
+    else
+    {
+  		while(fgets(temp, 512, fp) != NULL) {
+  		    if((strstr(temp, str)) != NULL) {
+
+  			  	if(flags.lMode){
+  			     	printf("%s \n", fname);
+  			      break;
+  			    }
+  			    else if(flags.cMode)
+  			    {
+  			     	lineCount++;
+  			    }
+  			    else
+  			    {
+  			      if(flags.nMode){ printf("%d:",line_num);}
+  			     	printf("%s", temp);
+  			    }
+
+  			    find_result++;
+
+  		    }
+
+  		 	line_num++;
+  		}
+
+  	 if(flags.cMode){ printf("%d \n",lineCount);}
+   }
+	}
+
+  if(find_result == 0) {
+    //do nothing
+  }
+
+  //Close the file if still open.
+  if(fp) {
+    fclose(fp);
+  }
+    return(0);
+}
+
+void searchDir(char* path, char* str){
+
+  struct stat fileStatus;
+  struct dirent *fileEntry;
+  DIR* dp;
+  pid_t pid;
+  if (stat(path,&fileStatus) != 0) printf("stat error\n");
+  if (S_ISDIR(fileStatus.st_mode)){
+
+    if ((dp = opendir(path)) == NULL) printf("Error opendir\n"); else{
+      char* path = getcwd(NULL, 256);
+      printf("entrei %s \n", path);
+    }
+
+    char newPath[256];
+
+    while ((fileEntry = readdir(dp)) != NULL){
+
+      //Ignore the current directory and previous directory folders.
+      if (strcmp(fileEntry->d_name, ".") == 0 || strcmp(fileEntry->d_name, "..") == 0){
+  
+        continue;
+      }
+
+      //Update the current path.
+      strcpy(newPath, path);
+      strcat(newPath, "/");
+      strcat(newPath, fileEntry->d_name);
+      stat(newPath, &fileStatus);
+  
+
+      //Whether the read structure is a file.
+      if (!S_ISDIR(fileStatus.st_mode)){
+  
+        searchInFile(fileEntry->d_name, str);
+   
+      }
+
+      //Whether the read structure is a directory.
+      if (S_ISDIR(fileStatus.st_mode)){
+      
+        switch (pid = fork()){
+          case -1:
+      
+            perror("Error fork()...\n");
+            break;
+
+          case 0: //child process
+          
+            if(flags.rMode==1){
+           
+            searchDir(newPath,str);}
+            break;
+
+
+
+        }
+      }
+    }
+    closedir(dp);
+  }
+
+}
+
+
+
+
+int main(int argc, char* argv[]){
+
+/
+	processArgs(argc,argv);
+	if(flags.rMode){
+		searchDir(argv[flags.fileNameNr], argv[flags.patternNr]);
+	}else{
+		searchInFile(argv[flags.fileNameNr], argv[flags.patternNr]);
+	}
+  
+  return 0;
+}
